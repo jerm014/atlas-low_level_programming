84f2b706d360b406989a19fb9c6bb7fbb54c8345
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-20 04:10:40 +0000

    Sat Jan 20 04:10:40 UTC 2024

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/*
+ * main - print a string
+ *
+ * Return: Always return 0
+ */
+
 int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
