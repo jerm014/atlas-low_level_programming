389d17a0ad750860c4883e8fd743a71806c86767
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-21 22:18:10 +0000

    Sun Jan 21 22:18:10 UTC 2024

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* The output of the program should be:
-		The number, followed by
-			if the number is greater than 0: is positive
-			if the number is 0: is zero
-			if the number is less than 0: is negative
-		followed by a new line
-	*/
-	if ( n>0 ) 
+	/**
+	 * The output of the program should be:
+	 *	The number, followed by
+	 *		if the number is greater than 0: is positive
+	 *		if the number is 0: is zero
+	 *		if the number is less than 0: is negative
+	 *	followed by a new line
+	 */
+
+	if ( n>0 )
 	{
 		printf("%d is positive\n", n);
 	}
