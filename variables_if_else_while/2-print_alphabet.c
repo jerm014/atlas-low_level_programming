f054690e6e338d8483ad4cce0c1139f4fa684667
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-21 23:22:41 +0000

    Sun Jan 21 23:22:41 UTC 2024

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Successful, always
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 97; n < 123; n++)
+		putchar(n);
+
+	putchar(10);
+
+	return (0);
+}
