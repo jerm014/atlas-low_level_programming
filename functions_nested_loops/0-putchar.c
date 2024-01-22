d7f315fc54d5ada83fb8b15ab614d7f6204e380f
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-22 00:24:08 +0000

    Mon Jan 22 00:24:08 UTC 2024

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Successful, always
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
