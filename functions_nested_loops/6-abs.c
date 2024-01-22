4ffddaf44c660a5d57c9f22d5807af2ba53528c0
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-22 01:48:21 +0000

    Mon Jan 22 01:48:21 UTC 2024

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * print_alphabet - print a to z
- *
- */
-
-void print_alphabet(void)
-{
-	int a = 97;
-	int z = 122;
-	int nn;
-
-	for (nn = a; nn <= z; nn++)
-	{
-		_putchar(nn);
-	}
-	_putchar('\n');
-}
-
-/**
  * print_alphabet_x10 - print the alphabet ten times
  *
  */
