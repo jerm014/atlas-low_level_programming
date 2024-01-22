145994830d270356ff4faa817d5fdf881a17e62b
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-22 00:03:16 +0000

    Mon Jan 22 00:03:16 UTC 2024

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Successful, always
+ */
+
+int main(void)
+{
+	int n;
+	int comma = 44;
+	int zero = 48;
+	int nine = 57;
+
+	for (n = zero; n <= nine; n++)
+	{
+		putchar(n);
+		if (n < nine)
+			putchar(comma);
+	}
+
+	putchar(10);
+
+	return (0);
+}
