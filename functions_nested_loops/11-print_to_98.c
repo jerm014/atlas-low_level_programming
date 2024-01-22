fb88ca18d512cc51ce738f24a38b35e9c51d8c11
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-22 03:47:58 +0000

    Mon Jan 22 03:47:58 UTC 2024

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new 
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new
  *               line
  *
  * @n: the number to start at
