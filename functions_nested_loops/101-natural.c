aaa1c08fbe219ba154db12ad9349029dfd21e82d
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-23 01:36:57 +0000

    Tue Jan 23 01:36:57 UTC 2024

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+  
+/**
+ * main - main entry point
+ *
+ * Return: nothing
+ *
+ */
+
+int main()
+{
+	int outcome[1024];
+	int a;
+	int sum_of_factors;
+
+	sum_of_factors = 0;
+    
+	for (a = 1; a <= 1024; a++)
+		outcome[a] = 0;
+        
+ 	for (a = 1; a <= 1024; a++)
+	{
+		if ((3 * a) <= 1024)
+		{
+			outcome[(3 * a)] = 1;
+		}
+		if ((5 * a) <= 1024)
+		{
+			outcome[(5 * a)] = 1;
+		}
+	}
+
+	for (a = 1; a <=1024; a++)
+	{
+		sum_of_factors += (a * outcome[a]);
+	}
+	printf("%d\n", sum_of_factors);
+}
