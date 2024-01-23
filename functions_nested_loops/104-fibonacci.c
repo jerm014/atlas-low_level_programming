90275e6043f3c8a9072a11856e032ed0dd6651f3
jerm014 <jermitts@gmail.com>
2024-01-22 23:24:10 -0600

    Update 104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -10,23 +10,23 @@
 int main(void)
 {
 	int count;
-	unsigned long f1 = 0, f2 = 1, fsum;
+	unsigned long fOne = 0, fTwo = 1, sum;
 	unsigned long fOneHalfOne, fOneHalfTwo, fTwoHalfOne, fTwoHalfTwo;
 	unsigned long halfOne, halfTwo;
 
 	for (count = 0; count < 92; count++)
 	{
-		fsum = f1 + f2;
-		printf("%lu, ", fsum);
+		sum = fOne + fTwo;
+		printf("%lu, ", sum);
 
-		f1 = f2;
-		f1 = fsum;
+		fOne = fTwo;
+		fTwo = sum;
 	}
 
-	fOneHalfOne = f1 / 10000000000;
-	fTwoHalfOne = f2 / 10000000000;
-	fOneHalfTwo = f1 % 10000000000;
-	fTwoHalfTwo = f2 % 10000000000;
+	fOneHalfOne = fOne / 10000000000;
+	fTwoHalfOne = fTwo / 10000000000;
+	fOneHalfTwo = fOne % 10000000000;
+	fTwoHalfTwo = fTwo % 10000000000;
 
 	for (count = 93; count < 99; count++)
 	{
