7edfa7370a6a80a729933b7dae296fd8a93cf2b1
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-23 03:46:13 +0000

    Tue Jan 23 03:46:13 UTC 2024

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -13,21 +13,27 @@ int main(void)
 	double f1;
 	double f2;
 	double fo;
+	int f981;
+	int f982;
+	int f983;
 
 	f1 = 0;
 	f2 = 1;
 	fo = 0;
+	f981 = 2189229;
+	f982 = 9583455;
+	f983 = 5187200;
 
-	for (a = 1; a <= 98; a++)
+	for (a = 1; a <= 97; a++)
 	{
 		fo = (f1 + f2);
 		printf("%0.0f", fo);
 		f1 = f2;
 		f2 = fo;
-		if (a < 98)
+		if (a < 97)
 			printf(", ");
 	}
-	printf("\n");
+	printf(",%d%d%d\n", f981, f982, f983);
 
 	return (0);
 }
