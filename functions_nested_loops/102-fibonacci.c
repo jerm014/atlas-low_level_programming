65806829d1b91e8700530f9eadbfb169710af1b7
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-23 01:42:55 +0000

    Tue Jan 23 01:42:55 UTC 2024

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Return: 1 always
+ *
+ */
+
+int main(void)
+{
+	int a;
+	int f;
+
+	f = 0;
+
+	for (a = 1; a <= 50; a++)
+	{
+		f =+ a;
+		printf("%d, \n", f);
+	}
+
+	return (1);
+}
