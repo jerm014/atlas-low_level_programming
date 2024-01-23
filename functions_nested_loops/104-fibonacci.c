f451249947421c366a0ddf51b3125898ce969c86
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-23 03:14:27 +0000

    Tue Jan 23 03:14:27 UTC 2024

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Return: 0 always
+ *
+ */
+
+int main(void)
+{
+	int a;
+	long f1;
+	long f2;
+	long fo;
+
+	f1 = 0;
+	f2 = 1;
+	fo = 0;
+
+	for (a = 1; a <= 98; a++)
+	{
+		fo = (f1 + f2);
+		printf("%ld", fo);
+		f1 = f2;
+		f2 = fo;
+		if (a < 50)
+			printf(", ");
+	}
+	printf("\n");
+
+	return (0);
+}
