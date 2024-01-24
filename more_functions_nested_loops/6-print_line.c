17476d942f8a3478d7fae8bd96353a5e3a8caf61
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-24 18:40:17 +0000

    fixed the 0 and less than 0 length lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,9 +2,8 @@
 
 /**
  * print_line- function that prints a line that is n characters long
- *             except if n is zero print a 1 character line, weird
  *
- * @n:         the length of the line- except 0 and 1 are both 1
+ * @n:         the length of the line
  *
  * Return:     nothing
  *
@@ -14,12 +13,12 @@ void print_line(int lineLength)
 {
 	int count;
 
-	if (lineLength == 0)
-		lineLength = 1;
-
-	for (count = 1; count <= lineLength; count++)
+	if (lineLength > 0)
 	{
-		_putchar('_');
+		for (count = 1; count <= lineLength; count++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 
