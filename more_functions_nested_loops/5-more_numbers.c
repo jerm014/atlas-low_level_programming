e99de125feb0c3cb3d549cc0c64904304af8da2e
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-24 18:24:47 +0000

    number 5 after helping Dan work his way through it...

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers- function that prints 10 times the numbers, from 0 to 14,
+ *               followed by a new line.
+ *
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int outerCount;
+	int innerCount;
+
+	for (outerCount = 1; outerCount <= 10; outerCount++)
+	{
+		for (innerCount = 0; innerCount <= 14; innerCount++)
+		{
+			if(innerCount >= 10)
+			{
+				_putchar((innerCount / 10) + '0');
+			}
+			_putchar((innerCount % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
