85fe752d1798805f8ae819dac3209e61180c137b
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-24 17:32:22 +0000

    2-mul first attempt

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ *
+ * @num1: a number to multiply with num2
+ * @num2: the number to multiply with num1
+ *
+ * Return: 0 (is much success!)
+ *         
+ */
+
+int mul(int num1, int num2)
+{
+	int res = (num1 * mum2);
+	_putchar(res);
+}
