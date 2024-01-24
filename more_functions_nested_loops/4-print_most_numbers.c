1b63f079b1948dbb1e4a486431d6d7362280ff34
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-24 17:44:51 +0000

    number 4 first attempt

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- print 0 through 9 but not 2 or 4
+ *
+ * Return: void; no return
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		if (count != 2 && count != 4)
+		{
+			_putchar(count+'0');
+		}
+	}
+}
