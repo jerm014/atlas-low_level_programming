43abdcce1f54e8d1d723697ad909b42207fa3faf
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-29 19:13:13 +0000

    first attempt

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts- print an entire string
+ *
+ * @str: the string to print
+ *
+ * Return: nothing
+ *
+ */
+
+void _puts(char *str)
+{
+	int pos = 0;
+
+	while (str[pos] != 0)
+	{
+		if (pos % 2 = 0)
+			_putchar(str[pos]);
+		pos++;
+	}
+	_putchar('\n');
+}
