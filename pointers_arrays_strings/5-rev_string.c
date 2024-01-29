f5f3f3f7a60d15d6057f5ac57a2ae34dbcdb1049
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-29 03:17:45 +0000

    first attempt of 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * rev_string- reverse a string
+ *
+ * @str: the string to reverse
+ *
+ * Return: reverse'd string
+ *
+ */
+
+void rev_string(char *str)
+{
+	int len;
+	int pos;
+	int strpos = 0;
+	char *out = str;
+
+	len = _strlen(str);
+
+	for (pos = (len - 1); pos >= 0; pos--)
+	{
+		str[strpos] = out[pos];
+		strpos++;
+	}
+
+}
+
+
+/**
+ * _strlen-  determine the length of a string
+ *
+ * @str:       the string to find the longness of
+ *
+ * Return:   the longness of the string
+ *
+ */
+
+int _strlen(char *str)
+{
+	int pos = 0;
+
+	while (str[pos] != 0)
+		pos++;
+
+	return (pos);
+}
