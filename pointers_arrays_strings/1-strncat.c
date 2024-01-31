9664457b56f2f9894f7b41aef4777cdfa8c4f73b
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-31 20:55:14 +0000

    wrong

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@
  *
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	int d = 0; /* keep track of how far into dest we move */
 	int s = 0; /* keep track of how far into src we move */
