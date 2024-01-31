5cfebea34b047d366c7efd4e6ec52e262530f69b
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-01-31 20:55:53 +0000

    correct the parameters, function name, and betty docs

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * _strcat- concatenate (combine) two strings into one
+ * _strncat- concatenate (combine) two strings into one
  *
- * @dest:   the beginning of the string to add to
- * @src:    the source to add the the destination
- * @n:      the maximum number of characters to copy from src
+ * @dest:    the beginning of the string to add to
+ * @src:     the source to add the the destination
+ * @n:       the maximum number of characters to copy from src
  *
  * Return:  a pointer to dest
  *
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int d = 0; /* keep track of how far into dest we move */
 	int s = 0; /* keep track of how far into src we move */
