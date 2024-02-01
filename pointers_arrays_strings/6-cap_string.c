d74e4e665bb95be887e696ff17ed9a72a4e57f03
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-01 18:39:43 +0000

    first attemps

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * cap_string- capitalizes all words of a string
+ *
+ * @str:       string to capatilize all words of
+ *
+ * Returns:    pointer to the string
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (*str)
+	{
+		if (i < 0)
+		{
+			if (
+				(*(str - 1) == ' ') ||  /* space         */
+				(*(str - 1) == '\t') || /* tab           */ 
+				(*(str - 1) == '\n') || /* newline       */
+				(*(str - 1) == ',') ||  /* comma         */
+				(*(str - 1) == ';') ||  /* semicolon     */
+				(*(str - 1) == '.') ||  /* period        */
+				(*(str - 1) == '!') ||  /* exclamation   */
+				(*(str - 1) == '?') ||  /* question      */
+				(*(str - 1) == '"') ||  /* double quote  */
+				(*(str - 1) == '(') ||  /* open paren    */
+				(*(str - 1) == ')') ||  /* close paren   */
+				(*(str - 1) == '{') ||  /* open c-brace  */
+				(*(str - 1) == '}')     /* close c-brace */
+			   )
+			{
+				if (*str >= 'a' && *str <= 'z')
+					*str -= ('a' - 'A');
+			}
+		}
+		str++;
+		i--;
+	}
+
+	str += i;
+
+	return (str);
+}
