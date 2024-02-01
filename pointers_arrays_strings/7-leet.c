127219f42eb8e7653bd9ae2c308b9ca36b417c98
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-01 19:15:44 +0000

    removed block comment that confuses betty-doc

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,14 +11,6 @@
 
 char *leet(char *str)
 {
-/**
- * Letters a and A should be replaced by 4
- * Letters e and E should be replaced by 3
- * Letters o and O should be replaced by 0
- * Letters t and T should be replaced by 7
- * Letters l and L should be replaced by 1
- *
- */
 	int a;
 	int i = 0;
 	char translate[10][2] = {{'a', '4'},
