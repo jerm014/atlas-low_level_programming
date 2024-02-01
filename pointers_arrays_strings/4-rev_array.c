151f9ab746a6804b0f6b503fe14e8426e4c30219
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-01 16:48:04 +0000

    deal with a zero len array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -15,10 +15,11 @@ void reverse_array(int *a, int n)
 	int i;
 	int tmp;
 
-	for (i = 0; i <= (n / 2); i++)
-	{
-		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
-	}
+	if (n > 0)
+		for (i = 0; i <= (n / 2); i++)
+		{
+			tmp = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = tmp;
+		}
 }
