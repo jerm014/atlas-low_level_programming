d12aee04f3adccb2bb6d09f845b5c003ce69dbfe
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-05 22:42:15 +0000

    first attemps

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
- *  -
+ * _strchr - function that locates a character in a string
  *
- *  @ :
+ * @s:       pointer to string to search
+ * @c:       character to search for in s
  *
- *  Return:
+ * Return:   a pointer to the first occurrence of the character c in the 
+ *           string s, or NULL if the character is not found
  *
  */
 
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if ((*s) == c)
+			return (s);
+		s++;
+	}
 
+	return (0);
+
+}
