421391a60002b97bb7f58c6cb5a015fdbcc46e52
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-05 22:00:23 +0000

    first attempt!

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
 /**
- *  -
+ * _memset - Write a function that fills memory with a constant byte
  *
- *  @ :
+ *  @s : a pointer to the char to fill with byte b
+ *  @b : a byte to copy n times into s
+ *  @n : the number of bytes to copy into s
  *
- *  Return:
+ *  Return: pointer to s
  *
  */
 
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = n;
 
+	while (n > 0)
+	{
+		(*s) = b;
+		s++;
+		n--;
+	}
+
+	s -= n;
+
+	return (s);
+
+}
