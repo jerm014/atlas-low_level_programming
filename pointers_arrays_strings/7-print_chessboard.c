f148163095fe94047389d7fbc9bfed46dbf395ec
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-06 02:35:25 +0000

    chess

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
- *  -
+ * print_chessboard - print a chessboard
  *
- *  @ :
+ * @a :               the chessboard
  *
- *  Return:
+ * Return:            nothing
  *
  */
 
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
 
+	for (row = 0; row <= 8; row++)
+	{
+		for (col = 0; col <= 8; col++)
+		{
+			_putchar(a[0][0]);
+		}
+		_putchar('\n');
+	}
+
+}
