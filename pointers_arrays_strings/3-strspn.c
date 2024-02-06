9df6f0ded0a88268cdcfaa9296b10aee343a8816
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-06 00:03:45 +0000

    thank you apple?

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
 /**
- *  -
+ * _strspn - return substring something something
  *
- *  @ :
- *
- *  Return:
+ * @s1 : string to search
+ * @s2 : accept
+ * 
+ * Return: how many bytes match
  *
  */
+unsigned int _strspn(char *s1, char *s2)
+{
+	register const char *p = s1, *spanp;
+	register char c, sc;
 
-
+	/*
+	 * Skip any characters in s2, excluding the terminating \0.
+	 */
+cont:
+	c = *p++;
+	for (spanp = s2; (sc = *spanp++) != 0;)
+		if (sc == c)
+			goto cont;
+	return (p - 1 - s1);
+}
