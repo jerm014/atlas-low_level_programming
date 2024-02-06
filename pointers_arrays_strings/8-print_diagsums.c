a2de6b3d9f0e1c4688806ebf88d27a0c5f0b5b62
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-06 04:12:14 +0000

    I hope this gets it

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,30 +13,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int left = 0;
-	int right = 0;
 	int leftSum = 0;
 	int rightSum = 0;
 	int position;
 
 	for (position = 0; position < size * size; position++)
 	{
-		printf("position is %d\n", position);
 		if ((position % (size + 1)) == 0)
-		{
-			printf("   left: the %% of position and size %d + 1 was zero, adding %d\n",
-					size, a[position]);
 			leftSum += a[position];
-			printf("   left: leftsum is now %d\n", leftSum);
-		}
+
 		if (((position % (size - 1)) == 0) && (position > 0) && (position < ((size * size) - (size - 1))))
-		{
-			printf("      right: the %% of position and size %d - 1 was zero, adding %d\n",
-					size, a[position]);
 			rightSum += a[position];
-			printf("      right: rightsum is now %d\n", rightSum);
-		}
-		
+
 	}
 
 	printf("%d, %d\n", leftSum, rightSum);
