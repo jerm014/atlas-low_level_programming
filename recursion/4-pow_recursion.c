8d08ad5e597de3b702b5ac2a7f94b3c326897bb2
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-08 23:06:00 +0000

    the 0 case is confusing

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -12,8 +12,6 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 1)
-		return (1);
 
 	if (y < 0)
 		return (-1);
@@ -24,6 +22,9 @@ int _pow_recursion(int x, int y)
 		x *= _pow_recursion(x, y);
 	}
 
-	return (x);
+	if (y == 0)
+		return (1);
+	else
+		return (x);
 
 }
