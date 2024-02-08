92f63144a5f3a8cc99398bac9f50a7faa4420b06
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-08 23:37:29 +0000

    gcc says its good

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,43 @@
 #include "main.h"
 
+int _sqrt(int, int);
+
 /**
- * -
+ * _sqrt_recursion- find the natural square root of a number
  *
- * @:
+ * @n:              the number to find the natural sq of
  *
- * Returns: 
+ * Return:          natural square root or -1 if there isn't one
  *
  */
 
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 0));
+}
 
+/**
+ * _sqrt-  do the grunt work of finding a square root
+ *
+ * @a:     the thing to find
+ * @b:     the number we're testing
+ *
+ * Return: the natural sq rt of a or the next iterator of b
+ *
+ */
+
+int _sqrt(int a, int b)
+{
+	if ((a == b) || (a < 0)) /* no find */
+		return (-1);
+
+	if (( a == 0) || (a == 1)) /* deal with 0 and 1 */
+		return (a);
+
+	if ((b * b) == a) /* found it! */
+		return (b);
+
+	/* else */
+	
+	return (_sqrt(a, (b + 1)));
+}
