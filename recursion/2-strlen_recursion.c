3e1e2fbadf2a36d12f0a58978598cd143e6e3281
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-08 22:35:13 +0000

    first attempt...

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
- * -
+ * _strlen_recursion- return the length of a string using recursion
  *
- * @:
+ * @s:                the string to measure
  *
- * Returns: 
+ * Returns:           length of the string
  *
  */
 
+int _strlen_recursion(char *s)
+{
+	int i = 0;
 
+	if (*s != 0)
+		return (i + _strlen_recursion(++s) + 1);
+	else
+		return (i);
+
+}
