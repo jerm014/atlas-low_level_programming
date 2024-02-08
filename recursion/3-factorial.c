bdb38de291b61826f9fa8f9c4b29ea9ee2d90804
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-08 22:46:56 +0000

    first attempt!!

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
- * -
+ * factorial- return the factroial of a number
  *
- * @:
+ * @n:        the number to calculate factorial of
  *
- * Returns: 
+ * Return:    factorial of n
  *
  */
 
-
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+	       return (-1);
+	else	
+	{
+		n *= factorial(n-1);
+		return (n);
+	}
+}
