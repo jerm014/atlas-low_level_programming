d089271a6ad91ee5e1f8a1993901e522c8c45be3
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-09 16:54:04 +0000

    first attempt

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-   main entry point; display the name of the executable
+ *
+ * @argc:  length of argv
+ * @argv:  arguments passed to executable
+ *
+ * Return: 0 success
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%d\n", --argc);
+	return (0);
+}
