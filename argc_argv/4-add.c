5522c74153ab29a27f8695d2b677ed244b00a676
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-09 20:27:37 +0000

    first attempt

---
+++ argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isNumeric(char *);
+
+/**
+ * main-   main entry point; add numbers up; display sum
+ *
+ * @argc:  length of argv
+ * @argv:  arguments passed to executable
+ *
+ * Return: 0 success or 1 for error
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i;
+	int res = 0;
+
+	
+	for (i = 1; i < argc; i++)
+	{
+		if (!isNumeric(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res += atoi(argv[i]);
+	}
+
+	printf("%d\n", res);
+	return (0);
+
+}
+
+/**
+ * isNumeric- determine if a string has anything besides numbers, -, and +
+ *
+ * @str:      the string to check
+ *
+ * Return:    1 true; 0 false
+ *
+ */
+
+int isNumeric(char *str)
+{
+    while(*str != '\0')
+    {
+        if((*str < '0' || *str > '9') && (*str != '-') && (*str != '+'))
+            return (0);
+        str++;
+    }
+    return (1);
+}
