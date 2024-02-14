067b41df2a539a0620a6c16668fa692b7b6870d9
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-14 18:24:20 +0000

    testing go

--- go
+++ go
@@ -4,12 +4,20 @@
 echo -e "\n>>> git add --all <<<\n"
 git add --all
 #chmod u+x $1
-echo -n "Commit Message: "
-read
-# next line needed for windows
-# git update-index --chmod=+x $2
-echo -e "\n>>>  git commit -m \"$REPLY\" <<<\n"
-git commit -m "$REPLY"
+
+if ["$1" == ""]; then
+	echo -n "Commit Message: "
+	read
+	# next line needed for windows
+	# git update-index --chmod=+x $2
+	echo -e "\n>>>  git commit -m \"$REPLY\" <<<\n"
+	git commit -m "$REPLY"
+fi
+if [ "$1"!="" ]; then
+	echo -e "\n>>> git commit -m \"$1\" <<<\n"
+	git commit -m "$1"
+fi
+
 echo -e "\n>>> git push <<<\n"
 git push
 echo ""
---
+++ malloc_free/1-strdmp.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _strdup -  a function that returns a pointer to a newly allocated space in
+ *            memory, which contains a copy of the string given as a parameter
+ *
+ * @str:      the string to put in the newly allocated space in memory
+ *
+ * Return:    NULL if str is NULL, otherwise a pointer to the duplicated string
+ *
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	unsigned int len = _strlen(str) + 1;
+	char *res;
+
+	res = malloc(sizeof(char) * i + 1);
+
+	if (res == NULL)
+		return (NULL);
+
+	for (; i < len; i++)
+		res[i] = str[i];
+
+	return (res);
+}
+
+/**
+ * _strlen-  determine the length of a string
+ *
+ * @s:       the string to find the longness of
+ *
+ * Return:   the longness of the string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int pos = 0;
+
+	while (s[pos] != 0)
+		pos++;
+
+	return (pos);
+}
