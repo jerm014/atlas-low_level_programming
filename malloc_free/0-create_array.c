9acdd05def819894b52999837f8ae6f112fcc927
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-14 17:37:16 +0000

    malloc first attempt

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_array - create an array of characters, fill with a character
+ * 
+ * @size:          how big the char array should be
+ * @c:             which character to fill the array with
+ *
+ * Return:         a pointer to the array or NULL if size = is 0
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *res;
+	int i = 0;
+
+	res = malloc(size + 1);
+
+	if (res == NULL)
+		return (NULL);
+
+	for (; i < size; i++)
+		res[i] = c;
+	
+	res[i] = '\0';
+
+	return (res);
+}
