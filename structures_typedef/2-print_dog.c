ae6bd35c1a7d592450981e68cb85714d467d55d4
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-21 06:55:38 +0000

    age?

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -28,10 +28,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age)
+		/* if (d->age) */
 			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
+		/* else */
+		/*	printf("Age: (nil)\n"); */
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
