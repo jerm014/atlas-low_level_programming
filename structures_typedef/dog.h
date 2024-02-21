a8e202d2ca84beff4dcaa55bbed41584251274fc
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-21 06:20:11 +0000

    first

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog information
+ *
+ * @name:       name of dog
+ * @age:        age of dog in human years
+ * @owner:      name of dog daddy or mommy
+ *
+ * Description: a structure for holding data about doge
+ *
+ */
+struct dog {
+	char *name,
+	float age,
+	char *owner,
+};
+#endif
