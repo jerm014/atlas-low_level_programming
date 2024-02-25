b533bc08733bac712c8ed1d10a904f26e44954ec
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-25 04:13:33 +0000

    first attempt

--- variadic_functions/1-main.c
+++ variadic_functions/1-main.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * main - check the code
--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
-#include <stdargs.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_numbers- a function that prints numbers, followed by a new line.
@@ -12,7 +13,7 @@
  *                * If separator is NULL, don't print it
  *                * Print a new line at the end of your function
  *
- * @seperator:    the thing to print between the numbers
+ * @separator:    the thing to print between the numbers
  * @n:            the number of numbers to print
  *
  * Return:        nothing
@@ -21,5 +22,16 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list ap;
 
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if ((separator != NULL) && (i > 0))
+			printf("%s", separator);
+		printf("%d", va_arg(ap, int));
+	}
+	printf("\n");
 }
