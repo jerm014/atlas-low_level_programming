d6eff8d36a64421e579667299240501d44a4f791
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-25 03:57:01 +0000

    incremental

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdargs.h>
+
+/**
+ * print_numbers- a function that prints numbers, followed by a new line.
+ *
+ *                * Prototype: void print_numbers(const char *separator, const
+ *                             unsigned int n, ...);
+ *                * where separator is the string to be printed between numbers
+ *                * and n is the number of integers passed to the function
+ *                * You are allowed to use printf
+ *                * If separator is NULL, don't print it
+ *                * Print a new line at the end of your function
+ *
+ * @seperator:    the thing to print between the numbers
+ * @n:            the number of numbers to print
+ *
+ * Return:        nothing
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
