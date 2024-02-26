8ae48367390d589bb89f737fb427d60269d1ed60
jerm014 <jermitts@gmail.com>
2024-02-25 22:41:06 -0600

    make header guard match proper file name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTIONS_POINTERS_H_
-#define _FUNCTIONS_POINTERS_H_
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 #include <stddef.h>
 	void print_name(char *name, void (*f)(char *));
 	void array_iterator(int *array, size_t size, void (*action)(int));
