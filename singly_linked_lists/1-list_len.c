309b92af50ad3f5e85285832c8709e7456ebc6df
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-06 23:08:27 +0000

    betty documentation correction

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
+ * list_len- a function that returns the nuber of elements in a linked list_t
+ *           list
+ *
  * Write a function that returns the number of elements in a linked list_t
  * list.
  *
- * Prototype: size_t list_len(const list_t *h);
- *
- * list_len- a function that returns the nuber of elements in a linked list_t
- *           list
+ *  * Prototype: size_t list_len(const list_t *h);
  *
  * @node     the head node of the linked list
  *
