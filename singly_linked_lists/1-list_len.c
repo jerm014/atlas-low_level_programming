8ab95322c46fb894565511be5320f64d229693a3
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-06 23:05:48 +0000

    first attempt

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,10 +8,24 @@
  * list_len- a function that returns the nuber of elements in a linked list_t
  *           list
  *
- * @h        the head node of the linked list
+ * @node     the head node of the linked list
  *
  * Return:   the number of elements in the linked list
  *
  */
 
-size_t list_len(const list_t *h)
+size_t list_len(const list_t *node)
+{
+	int i = 0;
+
+	if (node == NULL)
+		return (0);
+
+	while (node)
+	{
+		i++;
+		node = node->next;
+	}
+
+	return (i);
+}
