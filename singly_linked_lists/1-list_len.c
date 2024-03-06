37f0cdf3fcccfcb664373d6269623ba530353997
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-06 23:07:08 +0000

    betty line length in documentation

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * Write a function that returns the number of elements in a linked list_t list.
+ * Write a function that returns the number of elements in a linked list_t
+ * list.
  *
  * Prototype: size_t list_len(const list_t *h);
  *
