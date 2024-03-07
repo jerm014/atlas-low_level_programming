e0d6a42d2a82dce68a37efa3c6ee008de11e278c
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 05:41:09 +0000

    first attempt on 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "lists.h"
 
 /**
- * Write a function that frees a list_t list.
- *
- * Prototype: void free_list(list_t *head);
- *
  * free_list- free a list_t list
+ *
+ *            Write a function that frees a list_t list.
+ *
+ *            * Prototype: void free_list(list_t *head);
  *
  * @head:     a list_t node to start with
  *
@@ -13,4 +13,15 @@
  *
  */
 
+void free_list(list_t *head)
+{
+	list_t *temp_node;
 
+	while (head)
+	{
+		temp_node = head;
+		head = head->next;
+		free(temp_node->str);
+		free(temp_node);
+	}
+}
