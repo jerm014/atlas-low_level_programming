c29d444ed3cbfe281432d16b8ccbe62abe4d7314
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 19:22:39 +0000

    first attempt at 6

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,27 @@
 #include "lists.h"
+
+/**
+ * pop_listint- a function that deletes the head node of a listint_t linked
+ *              list, and returned the head node's data
+ *
+ * @node:       the head node of a listint_t list
+ *
+ * Return:      n value from node
+ *
+ */
+
+int pop_listint(listint_t **node)
+{
+	listint_t *temp_node;
+	int n = 0;
+
+	if (node != NULL)
+	{
+		temp_node = *node;
+		n = temp_node->n;
+		*node = temp_node->next;
+		free(temp_node);
+	}
+
+	return (n);
+}
