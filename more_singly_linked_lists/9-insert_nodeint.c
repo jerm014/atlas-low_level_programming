4171ecfbdb5c494d568d99eebb8d3b346f3e175d
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 20:46:58 +0000

    first attempt at 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,54 @@
 #include "lists.h"
+
+/**
+ * insert_nodeint_at_index- a function that inserts a new node to a listint_t
+ *                          linked list at a given position
+ *
+ *                          Write a function that inserts a new node at a given
+ *                          position.
+ *
+ *                          * Prototype:
+ *                            listint_t *insert_nodeint_at_index(listint_t
+ *                            **head, unsigned int idx, int n);
+ *                          * where idx is the index of the list where the new
+ *                            node should be added. Index starts at 0
+ *                          * Returns: the address of the new node, or NULL if
+ *                            it failed
+ *                          * if it is not possible to add the new node at
+ *                            index idx, do not add the new node and return
+ *                            NULL
+ *
+ * @node:                   the head node of a listint_t linked list
+ * @index:                  the index at which to insert the new node
+ * @n:                      the data to put in the new node
+ *
+ * Return:                  the address of the new node or NULL if failure
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **node, unsigned int index, int n)
+{
+	unsigned int count;
+	listint_t *temp_node;
+	listint_t *new_node;
+
+	temp_node = *node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || temp_node == NULL)
+		return (NULL);
+
+	for (count = 0; count < index - 1; count++)
+	{
+		if (temp_node)
+			temp_node = temp_node->next;
+		else
+			return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = temp_node->next;
+	temp_node->next = new_node;
+
+	return (new_node);
+}
