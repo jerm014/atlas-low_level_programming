e893ee2e0b3f72d9cdb118775f570faa7932ff58
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 18:49:15 +0000

    first attempt at 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,42 @@
 #include "lists.h"
+
+/**
+ * free_listint2- free a listint_t list
+ *
+ *                Write a function that frees a listint_t list
+ *                * Prototype: void free_listint2(listint_t **node);
+ *                * The function sets the head to NULL
+ *
+ * @node:         the head node of a listint_t list
+ *
+ * Return:        nothing
+ *
+ */
+
+void free_listint2(listint_t **node)
+{
+	free_listint(*node);
+	*node = NULL;
+}
+
+
+/**
+ * free_listint- free a lintint_t list
+ *
+ * @node:        the head node of a listint_t list
+ *
+ * Return:       nothing
+ *
+ */
+
+void free_listint(listint_t *node)
+{
+	listint_t *temp_node;
+
+	while (node)
+	{
+		temp_node = node;
+		node = node->next;
+		free(temp_node);
+	}
+}
