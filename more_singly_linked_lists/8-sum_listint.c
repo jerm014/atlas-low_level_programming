a92c22a50a2d8b85a46984c728a2449ee4ea2319
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 20:17:34 +0000

    first attempt at 8

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,29 @@
 #include "lists.h"
+
+/**
+ * sum_listint- return the sum of all the data(n) of a listint_t linked list
+ *
+ *              Write a function that returns the sum of all the data (n) of a
+ *              listint_t linked list.
+ *
+ *              * Prototype: int sum_listint(listint_t *head);
+ *              * if the list is empty, return 0
+ *
+ * @node:       the head node of a list_t linked list
+ *
+ * Return:      the sum of all node->n in the linked list starting at node
+ *
+ */
+
+int sum_listint(listint_t *node)
+{
+	int sum = 0;
+
+	while (node)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+
+	return (sum);
+}
