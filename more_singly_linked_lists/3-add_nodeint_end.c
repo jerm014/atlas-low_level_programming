2bfb3c16f2c9732cb322bf24bf3cf864c93a784e
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 15:27:17 +0000

    first attempt on 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,1 +1,40 @@
 #include "lists.h"
+
+/**
+ * add_nodeint_end-  add a new node at the end of a listint_t list
+ *
+ * @node:            the node at the head of the lintint_t list
+ * @n:               the number to put in the node
+ *
+ * Return:           the new node or NULL if this failed
+ *
+ */
+
+listint_t *add_nodeint_end(listint_t **node, const int n)
+{
+	listint_t *new_node = NULL;
+	listint_t *temp_node;
+
+	if (node == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*node == NULL)
+		*node = new_node;
+	else
+	{
+		temp_node = *node;
+		while (temp_node->next)
+			temp_node = temp_node->next;
+
+		temp_node->next = new_node;
+	}
+
+	return (new_node);
+}
