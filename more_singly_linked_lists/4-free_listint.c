d6ebedf099a9edf0372f1a4baebd2639d7b3a24b
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-07 15:33:30 +0000

    first attempt on 4

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+
+/**
+ * free_listint- free a lintint_t list
+ *
+ * @node:        the head node of a listint_t list
+ *
+ * Return:       nothing
+ *
+ */
+
+void free_listint(listint_t *node)
+{
+	listint_t *temp_node;
+
+	while (node)
+	{
+		temp_node = node;
+		node = node->next;
+		free(temp_node);
+	}
+}
