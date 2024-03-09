a6b7a1a468f00f3989e0d167b77a476cfa1a2622
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-09 22:22:20 +0000

    handle top?

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -34,17 +34,20 @@ listint_t *insert_nodeint_at_index(listint_t **node, unsigned int index, int dat
 
 	temp_node = *node;
 
+	if (index == 0)
+		return(add_nodeint(*head, data));
+`
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL || temp_node == NULL)
 		return (NULL);
 
-	while ((count + 1 < index) && temp_node && (index != 0))
+	while ((count + 1 < index) && temp_node)
 	{
 		temp_node = temp_node->next;
 		count++;
         }
 
-	if ((count == index - 1) || (index == 0))
+	if (count == index - 1)
 	{
 		new_node->n = data;
 		new_node->next = temp_node->next;
@@ -52,4 +55,41 @@ listint_t *insert_nodeint_at_index(listint_t **node, unsigned int index, int dat
 	}
 
 	return (new_node);
+}
+
+/**
+ * add_nodeint- a function that adds a new node at the beginning of a listint_t
+ *              list
+ *
+ *              Write a function that adds a new node at the beginning of a
+ *              listint_t list.
+ *
+ *              * Prototype: listint_t *add_nodeint(listint_t **, const int);
+ *              * Returns:   the address of the new element, or NULL if it
+ *                           failed
+ *
+ * @node:      the head node of the listint_t list
+ * @n:         a number to put in the new node
+ *
+ * Return:    ` a pointer to the new element or NULL if it failed
+ *
+ */
+
+listint_t *add_nodeint(listint_t **node, const int n)
+{
+	listint_t *new_node = NULL;
+
+	if (node == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *node;
+
+	*node = new_node;
+
+	return (*node);
 }
