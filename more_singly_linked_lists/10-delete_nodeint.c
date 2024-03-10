c8f3130062c0f6dc8e550ff5285a88cd91c206a1
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-10 01:11:38 +0000

    handle n greater than list items

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **node, unsigned int index)
 		return (1);
 	}
 
-	while (count < index - 1)
+	while (count < index - 1 && temp_node)
 	{
 		temp_node = temp_node->next;
 		count++;
