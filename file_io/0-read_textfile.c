c3bd70a0bb79edec4b0b4c318a837429c69baa59
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-11 21:42:17 +0000

    first try

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile- function that reads a text file and prints it to the
+ *                POSIX standard output
+ *
+ * @filename:     the name of the file to print
+ * @letters:      how many bytes to read and print
+ *
+ * Return:        the actual number of bytes it as able to read and print
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	char ch;
+	size_t i;
+
+	if (!filename)
+		return (0);
+
+	fptr = fopen(filename, "r");
+
+	if (!fptr)
+		return (0);
+
+	for (i = 0; ((i < letters) && (ch != EOF)); i++)
+	{
+		ch = fgetc(fptr);
+		if (!ch)
+		{
+			fclose(fptr);
+			return (0);
+		}
+		write (1, &ch, 1);
+	}
+
+	fclose(fptr);
+	return (--i);
+
+}
