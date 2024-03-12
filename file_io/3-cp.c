8c33173736596222717c1d33cdfb00b4c56ea6e9
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-12 03:01:32 +0000

    found extra read and removed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,7 +4,7 @@
  * main-  main entry point for program
  *
  * @argc: count of arguments
- * @arvg: array of pointers to strings a argenemts
+ * @argv: array of pointers to strings a argenemts
  *
  * Return: 0 on success, 98 or 97 for other errors
  *
@@ -12,10 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int file_from;
-	int file_to;
-	int read_val = 1;
-	int write_val;
+	int file_from, file_to, read_val = 1, write_val;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -25,56 +22,82 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	read_val = read(file_from, buffer, 1024);
-	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
-		exit(98);
-	}
+		return (cant_read(argv[1], 98));
+	
+	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		cant_write(argv[2], 99);
+
 	while ((read_val = read(file_from, buffer, 1024)) > 0)
 	{
 		write_val = write(file_to, buffer, read_val);
 		if (write_val != read_val)
 		{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		
-		if (close(file_from) == -1)
-		{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
-		return (100);
-		}
-		if (close(file_to) == -1)
-		{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
-		return (100);
-		}
-		return (98);
+			cant_write(argv[2], 0);
+			if (close(file_from) == -1)
+				return (cant_close(argv[1], 100));
+
+			if (close(file_to) == -1)
+				return (cant_close(argv[2], 100));
+
+			return (98);
 		}
 	}
 
 	if (read_val == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
-		return (98);
-	}
-		
+		return (cant_read(argv[1], 98));
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
-		return (100);
-	}
+		return (cant_close(argv[1], 100));
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
-		return (100);
-	}
-
+		return (cant_close(argv[2], 100));
 	return (0);
 }
 
+/**
+ * cant_close- error can't close filename and return value
+ *
+ * @filename:  the name of the file that can't be closed
+ * @ret:       the return value
+ *
+ * Return:     int from @ret
+ *
+ */
+
+int cant_close(char *filename, int ret)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close %s\n", filename);
+	return (ret);
+}
+
+/**
+ * cant_write- error can't write to filename and return value
+ *
+ * @filename:  the name of the file that can't be written to
+ * @ret:       the return value
+ *
+ * Return:     int from @ret
+ *
+ */
+
+int cant_write(char *filename, int ret)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	return (ret);
+}
+
+/**
+ * cant_read- error can'tread to filename and return value
+ *
+ * @filename: the name of the file that can't be read from
+ * @ret:      the return value
+ *
+ * Return:    int from @ret
+ *
+ */
+
+int cant_read(char *filename, int ret)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", filename);
+	return (ret);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -10,5 +10,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int cant_read(char *filename, int ret);
+int cant_write(char *filename, int ret);
+int cant_close(char *filename, int ret);
 
 #endif
