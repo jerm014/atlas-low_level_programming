ed62a3b5d41879882e448dfaf941da0e2da6d939
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 23:06:32 +0000

    dlistint_len added to file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -49,3 +49,24 @@ dlistint_t
 
 	return (NULL);
 }
+
+/**
+ * dlistint_len- print all the elements of a dlistint_t list
+ *
+ * @h:             pointer to the head of the dlistint_t list
+ *
+ * Return:         the number of nodes
+ *
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
