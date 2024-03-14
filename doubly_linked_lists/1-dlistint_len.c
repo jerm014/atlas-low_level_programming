271955762277eb76a6b0607fcbd46887c954ddb6
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 02:23:00 +0000

    first try on 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+
+/**
+ * dlistint_len- print all the elements of a dlistint_t list
+ *
+ * @h:             pointer to the head of the dlistint_t list
+ *
+ * Return:         the number of nodes
+ *
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 1;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (--i);
+}
