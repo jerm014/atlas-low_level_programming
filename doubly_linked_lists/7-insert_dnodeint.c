7ab6dce18036d43233aa973ea60c583fd2b9b592
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 23:04:41 +0000

    remove debugging

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -49,36 +49,3 @@ dlistint_t
 
 	return (NULL);
 }
-
-#include "0-print_dlistint.c"
-#include "1-dlistint_len.c"
-#include "2-add_dnodeint.c"
-#include "3-add_dnodeint_end.c"
-#include "4-free_dlistint.c"
-
-/**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
-{
-    dlistint_t *head;
-
-    head = NULL;
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
-    print_dlistint(head);
-    printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 5, 4096);
-    print_dlistint(head);
-    free_dlistint(head);
-    head = NULL;
-    return (EXIT_SUCCESS);
-}
