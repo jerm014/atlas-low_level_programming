22eee8368ece9122b90a94f7c97aa12eecb1296a
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 20:16:51 +0000

    first attempt at 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,27 @@
 #include "lists.h"
+
+/**
+ * get_dnodeint_at_index- return the nth node of a dlistint_t linked list
+ *
+ * @node:                 the nead of the dlistint linked list
+ * @index:                the index of the node to return (0-based)
+ *
+ * Return:                a node from the dlistint_t
+ *
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *node, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while ((count != index) && (node->next))
+	{
+		node = node->next;
+		count++;
+	}
+
+	if (count == index)
+		return (node);
+
+	return (NULL);
+}
