19c8a8ff2f4595fd19ff811f48aa7c384970b76d
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 20:04:00 +0000

    first try with recursion

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+
+/**
+ * free_dlistint- free a dlistint_t list
+ * 
+ * @node:         the head node of a dlistint_t list
+ * 
+ * Return:        nothing
+ * 
+*/
+
+void free_dlistint(dlistint_t *node)
+{
+	if (node->next != NULL)
+		free_dlistint(node->next);
+
+	free(node);
+}
