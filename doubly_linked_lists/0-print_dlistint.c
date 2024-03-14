2669e7b693bf6fd1d6cc6bd6e98d2e2d7e021a20
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 02:18:08 +0000

    betty code

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,16 +2,17 @@
 
 /**
  * print_distinct- print all the elements of a dlistint_t list
- * 
+ *
  * *head:          pointer to the head of the dlistint_t list
- * 
+ *
  * Return:         the number of nodes
- * 
+ *
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 1;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
