e8d2a55371ab563ce8f0e0f95a57a6ee3f2b9b56
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 21:50:48 +0000

    first attempt at 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,1 +1,49 @@
 #include "lists.h"
+
+/**
+ * insert_dnodeint_at_index- insert a new node at a given index
+ *
+ * @node:                    the head of a dlistint_t linked list
+ * @index:                   the index to put the new node (0-based)
+ * @n:                       the value to store in the n of the new node
+ *
+ * Return:                   the address of the new node or NULL if it failed
+ *
+ */
+
+dlistint_t
+*insert_dnodeint_at_index(dlistint_t **node, unsigned int index, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *temp_node;
+	dlistint_t *new_node;
+
+	temp_node = *node;
+
+	if (index == 0)
+		return (add_dnodeint(node, n));
+
+	while ((count + 1 < index) && temp_node)
+	{
+		temp_node = temp_node->next;
+		count++;
+	}
+
+	if (count == index - 1)
+	{
+		new_node->n = n;
+		new_node->next = temp_node->next;
+		new_node->prev = temp_node;
+		temp_node->next = new_node;
+
+		if (temp_node->next)
+		{
+			temp_node = temp_node->next;
+			temp_node->prev = new_node;
+		}
+		
+		return (new_node);
+	}
+
+	return (NULL);
+}
