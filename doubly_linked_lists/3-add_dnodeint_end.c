706a8a8b5a309d689629892f8a98cf7965f9fbee
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 19:27:19 +0000

    first try at 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,1 +1,38 @@
 #include "lists.h"
+
+/**
+ * add_dnodeint_end- add a new node at the end of a dlistint_t
+ *
+ * @head:            the head of the linked list
+ * @n:               the n value of the new node
+ *
+ * Return:           the address of the new node or NULL if it failed
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **node, const int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *temp_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL || node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = new_node->prev = NULL;
+
+	if (*node == NULL)
+		*node = new_node;
+	else
+	{
+		temp_node = *node;
+		while (temp_node->next)
+			temp_node = temp_node->next;
+		
+		new_node->prev = temp_node;
+		temp_node->next = new_node;
+	}
+
+	return (new_node);
+}
