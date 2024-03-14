bad55808e49206a23d4b57a56fcf043e9a7b3316
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-14 20:28:23 +0000

    first attempt at 6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+
+/**
+ * sum_dlistint- return the sum of all the data (n) of a dlistint_t linked list
+ *
+ * @node:        the head node of a dlistint_t linked list
+ *
+ * Return:       int; the number of nodes
+ * 
+*/
+
+int sum_dlistint(dlistint_t *node)
+{
+	int sum = 0;
+
+	while (node)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+
+	return (sum);
+}
