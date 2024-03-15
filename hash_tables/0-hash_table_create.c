bd3e41af29aeb195de16bcf553a32c5b8c4ee704
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-15 16:06:31 +0000

    make sapce for the hash nodes.

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,12 +11,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *array;
+	hash_table_t *table;
 
-	array = malloc(sizeof(hash_table_t));
-	if (array == NULL)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 	
-	array->size = size;
-	return (array);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
+	
+	
+	return (table);
 }
