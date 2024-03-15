84d78e9941896eab5d5e1215f0984fe54fd5115b
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-15 14:46:59 +0000

    first try at 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,5 +13,36 @@
 
 int delete_dnodeint_at_index(dlistint_t **node, unsigned int index)
 {
-	
+	unsigned int count = 0;
+	dlistint_t *temp_node;
+	dlistint_t *new_node;
+
+	temp_node = *node;
+
+	if (index == 0)
+	{
+		(*node)->next->prev = NULL;
+		temp_node = *node;
+		(*node) = (*node)->next;
+		free(temp_node);
+		return (1);
+	}
+
+	while ((count != index) && temp_node)
+	{
+		temp_node = temp_node->next;
+		count++;
+	}
+
+	if (count == index)
+	{
+		temp_node->prev->next = temp_node->next;
+		if (temp_node->next)
+			temp_node->next->prev = temp_node->prev;
+		free(temp_node);
+
+		return (1);
+	}
+
+	return (-1);
 }
