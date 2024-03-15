c43aedd1fa7d05f6e738e3935e9ad705446ad3e7
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-15 18:11:29 +0000

    first attempt

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index- find the index for a given key
+ * 
+ * @key:      the key
+ * @size:     the size of the thing
+ * 
+ * Return:    unsigned long int index for key
+ * 
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
