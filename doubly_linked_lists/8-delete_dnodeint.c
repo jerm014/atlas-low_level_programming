2879f30342ebd1398a78609d5ecf760de78b71b0
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-15 04:42:13 +0000

    incremental

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+
+/**
+ * delete_dnodeint_at_index- delete the node at index of a dlistint_t linked
+ *                           list
+ *
+ * @node:                    the head of a dlistint_t linked list
+ * @index:                   the node to remove
+ *
+ * Return:                   1 success; -1 fail
+ *
+*/
+
+int delete_dnodeint_at_index(dlistint_t **node, unsigned int index)
+{
+	
+}
