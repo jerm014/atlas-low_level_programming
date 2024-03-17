5c78a600ee5e7130430c8c79d742083fc1369612
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-03-17 05:49:14 +0000

    incremental

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -21,13 +21,13 @@ int hash_table_set(hash_table_t *table, const char *key, const char *value)
 	if (new_node == NULL || table == NULL || key == NULL || value == NULL)
 		return (0);
 
-	index = key_index((const unsigned char*)key, table->size);
+	index = key_index((const unsigned char *)key, table->size);
 	temp_node = table->array[index];
 
 	new_node->key = malloc(strlen(key) + 1);
 	if (new_node->key == NULL)
 		return (0);
-	
+
 	new_node->value = malloc(strlen(value) + 1);
 	if (new_node->value == NULL)
 		return (0);
@@ -43,8 +43,53 @@ int hash_table_set(hash_table_t *table, const char *key, const char *value)
 		while (temp_node->next)
 			temp_node = temp_node->next;
 
-		temp_node->next = new_node;	
+		temp_node->next = new_node;
 	}
 
 	return (1);
+}
+
+/**
+ * add_node- a function that adds a new node at the beginning of a list_t list
+ *
+ *            Write a function that adds a new node at the beginning of a
+ *            list_t list.
+ *
+ *            * Prototype: list_t *add_node(list_t **head, const char *str);
+ *            * Returns: the address of the new element, or NULL if it failed
+ *            * str needs to be duplicated
+ *            * You are allowed to use strdup
+ *
+ * @head:     the top node of a list_t list
+ * @str:      a string to put in the new node
+ *
+ * Return:    a pointer to the new element
+ *
+ */
+
+hash_node_t *add_node(hash_node)t **node, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+
+	if (node == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = malloc(_strlenconst(key) + 1);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = _strlenconst(const);
+	_strcpyconst(new_node->value, value);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
 }
