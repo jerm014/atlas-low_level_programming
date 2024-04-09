5dc218204ea340c003f0bd42c949ba421c2477d0
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-09 19:36:41 +0000

    0 initial commit

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * linear_search- Function to perform linear search
+ *
+ * @array: the array
+ * @size:  size of the array
+ * @value: search for this
+ *
+ * Return: the index that value is found
+ *
+ */
+int linear_search(int array[], int size, int value);
+{
+	size_t i;
+
+	for (i = 0; i < n; i++) {
+		if (arr[i] == key) {
+			return (i);
+		}
+	}
+	return -1;
+}
