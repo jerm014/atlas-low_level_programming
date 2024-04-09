112a7791c054ef88a0bdc74d6e99d4927cd1eea6
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-09 19:42:39 +0000

    key -> value

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int array[], size_t size, int value)
 	size_t i;
 
 	for (i = 0; i < size; i++) {
-		if (array[i] == key)
+		if (array[i] == value)
 			return (i);
 	}
 	return -1;
