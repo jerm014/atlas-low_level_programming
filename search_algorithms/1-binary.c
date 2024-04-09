934535266238205f34c26588e0a6c560ac24534a
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-09 19:51:36 +0000

    1-binary initial

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * binary_search-  a function that searches for a value in a sorted array of
+ *                 integers using the Binary search algorithm
+ *
+ * @array:         the array to search
+ * @size:          the size of the array
+ * @value:         the value to find
+ *
+ * Return:         the index that the value is found at
+ *
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (0);
+}
