60fbd66ab437edb19a637337d1bef59b87d77be7
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-09 19:41:58 +0000

    arr -> array

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,9 +15,8 @@ int linear_search(int array[], size_t size, int value)
 	size_t i;
 
 	for (i = 0; i < size; i++) {
-		if (arr[i] == key) {
+		if (array[i] == key)
 			return (i);
-		}
 	}
 	return -1;
 }
