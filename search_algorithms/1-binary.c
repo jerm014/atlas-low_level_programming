d9726b43e177c35810f668eb45b7c7215fa89b80
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-10 19:52:13 +0000

    1 first attempt

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include "1-main.c"
 
 /**
  * binary_search-  a function that searches for a value in a sorted array of
@@ -13,5 +14,65 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	return (0);
+	return (binary_search_search(array, 0, size, value));
+}
+
+/**
+ * binary_search_search- recursive search for value in int array
+ *
+ * @array:               the ordered array to search
+ * @start:               the starting index for this search partition
+ * @end:                 the end index for this search partition
+ * @value:               the value to find in the array
+ *
+ * Return:               the position the value is found or -1 if not found
+ *
+*/
+
+int binary_search_search(int *array, size_t start, size_t end, int value)
+{
+	size_t half = ((end - start) / 2) + start;
+
+	if (!(array))
+		return (-1);
+
+	print_array(array, start, end);
+
+	if (array[half] == value)
+		return (half);
+
+	if (start != end)
+	{
+		if (value > array[half])
+			return (binary_search_search(array, (half + 1), end, value));
+		else
+			return (binary_search_search(array, start, (half - 1), value));
+	}
+
+	return (-1);
+}
+
+/**
+ * print_array- print the array being searched
+ *
+ * @array:      the array being searched
+ * @start:      the start index to search
+ * @end:        the end index to search
+ *
+ * Return:      nothing
+ *
+*/
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%lu", i);
+		if (i < end)
+			printf(", ");
+	}
+	printf("\n");
+
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,7 +1,11 @@
 #ifndef _SEARCH_
 #define _SEARCH_
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int array[], size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_search(int *array, size_t start, size_t end, int value);
+void print_array(int *array, size_t start, size_t end);
+
 #endif
